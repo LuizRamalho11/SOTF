11a8c1f485fdfc875123832f18a7f0ea023b02ee
LuyKoji <luykoji@gmail.com>
2026-05-26 21:12:35 -0300

    Remove transfer method from Conta class
    Removed transfer method and related test cases.

--- conta.py
+++ conta.py
@@ -75,16 +75,6 @@ class Conta:
             return
         self.__saldo -= valor
         print(f"Saque de R$ {valor} realizado. Novo saldo: R$ {self.__saldo}.")
-    def transferir(self, valor: float, conta_destino: 'Conta') -> None:
-        if valor <= 0:
-            print("Erro: O valor da transferência deve ser maior que zero.")
-            return
-        if valor > (self.__saldo + self.__limite):
-            print("Erro: Saldo e limite insuficiente para realizar a transferência.")
-            return
-        self.__saldo -= valor
-        conta_destino.depositar(valor)
-        print(f"Transferência de R$ {valor} realizada para a conta do tipo '{conta_destino.get_tipo()}.' Novo saldo: R$ {self.__saldo}.")
 
 # Blocos de demonstração
 if __name__ == "__main__":
@@ -108,11 +98,8 @@ print("\nTestando operações...")
 conta1.depositar(200)
 conta1.sacar(500)
 conta1.sacar(1000)
-conta1.transferir(300, conta2)
 print("\nDados da conta 1 após operações:")
 print(conta1)
-print(f"\nDados da conta 2 após receber transferência:")
-print(conta2)
 print("\nRemovendo uma conta...")
 del conta2
 print(f"Total de contas no sistema após remoção: {Conta.get_total_contas()}")
